b2f432184986cc523df8f851dc924bc6b085763d
xuchengpeng <330476629@qq.com>
2023-01-31 16:17:35 +0800

    add telescope oldfiles keybinding

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,6 +12,7 @@ return {
         { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
         { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find buffers" },
         { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
+        { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recents files" },
         { "<leader>ft", "<cmd>Telescope<cr>", desc = "Telescope" },
     },
 }
