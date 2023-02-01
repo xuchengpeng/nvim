82f92bbb794102ac216705f3607757d0adc67807
xuchengpeng <330476629@qq.com>
2023-02-01 11:41:27 +0800

    add zen-mode

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -7,4 +7,20 @@ return {
                 require("which-key").setup()
             end
     },
+    {
+        "folke/zen-mode.nvim",
+        lazy = true,
+        cmd = "ZenMode",
+        dependencies = { "folke/twilight.nvim" },
+        config = function()
+            require("zen-mode").setup({
+                window = {
+                    backdrop = 0.95,
+                    width = 0.8,
+                    height = 1,
+                },
+            })
+        end,
+        keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
+    }
 }
