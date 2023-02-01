86557b5d3e31ac3dc1fdb0e11ca5b1d8017038ee
xuchengpeng <330476629@qq.com>
2023-02-01 11:12:42 +0800

    load tokyonight during lazy installation

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -2,6 +2,7 @@ return {
     {
         "folke/tokyonight.nvim",
         lazy = false,
+        priority = 1000,
         config = function()
             local tokyonight = require("tokyonight")
             tokyonight.setup({
