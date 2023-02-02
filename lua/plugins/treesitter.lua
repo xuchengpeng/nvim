9af855e520cc5dc3b0384327f6cf67f51f7e96d0
xuchengpeng <330476629@qq.com>
2023-02-02 19:04:22 +0800

    show code context with tree-sitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,26 +1,34 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    lazy = true,
-    event = { "BufReadPost", "BufNewFile" },
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "c",
-                "cmake",
-                "cpp",
-                "help",
-                "lua",
-                "markdown",
-                "markdown_inline",
-                "python",
-                "vim",
-            },
-            sync_install = false,
-            auto_install = false,
-            highlight = {
-                enable = true,
-            }
-        })
-    end,
-    build = ":TSUpdate"
+    {
+        "nvim-treesitter/nvim-treesitter",
+        lazy = true,
+        event = { "BufReadPost", "BufNewFile" },
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {
+                    "c",
+                    "cmake",
+                    "cpp",
+                    "help",
+                    "lua",
+                    "markdown",
+                    "markdown_inline",
+                    "python",
+                    "vim",
+                },
+                sync_install = false,
+                auto_install = false,
+                highlight = {
+                    enable = true,
+                }
+            })
+        end,
+        build = ":TSUpdate",
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        lazy = true,
+        event = { "BufReadPre" },
+        config = true,
+    },
 }
