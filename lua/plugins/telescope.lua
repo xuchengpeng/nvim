11e29198e193ef7c3655331eaf03720cbc0c4099
xuchengpeng <330476629@qq.com>
2023-02-06 09:25:04 +0800

    add telescope extensions

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,17 +2,26 @@ return {
     "nvim-telescope/telescope.nvim",
     lazy = true,
     cmd = "Telescope",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope-file-browser.nvim",
+        "nvim-telescope/telescope-live-grep-args.nvim",
+    },
     config = function()
         require("telescope").setup()
+        require("telescope").load_extension("file_browser")
+        require("telescope").load_extension("live_grep_args")
     end,
     keys = {
         { "<leader>f", desc = "+Telescope" },
+        { "<leader>fb", "<cmd>Telescope file_browser<cr>", desc = "File browser" },
         { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
-        { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
-        { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find buffers" },
+        { "<leader>fg", "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<cr>",
+            desc = "Live grep (Args)" },
+        { "<leader>fG", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
         { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
         { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recents files" },
+        { "<leader>fs", "<cmd>Telescope buffers<cr>", desc = "Switch buffers" },
         { "<leader>ft", "<cmd>Telescope<cr>", desc = "Telescope" },
     },
 }
