88379cd5ee183e49832d8fe1a5dfad1b1fb8acb3
xuchengpeng <330476629@qq.com>
2023-02-18 16:08:07 +0800

    refactor lazy config

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,5 +1,9 @@
-require("config.defaults").load_defaults()
+local M = {}
 
-require("config.autocmds").load_defaults()
+function M.load()
+    require("config.defaults").load_defaults()
+    require("config.autocmds").load_defaults()
+    require("config.lazy").setup()
+end
 
-require("config.lazy")
+return M
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,19 +1,25 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
+local M = {}
+
+function M.setup()
+    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+    if not vim.loop.fs_stat(lazypath) then
+        vim.fn.system({
+            "git",
+            "clone",
+            "--filter=blob:none",
+            "https://github.com/folke/lazy.nvim.git",
+            "--branch=stable", -- latest stable release
+            lazypath,
+        })
+    end
+    vim.opt.rtp:prepend(lazypath)
+
+    require("lazy").setup({
+        spec = {
+            { import = "plugins" },
+        },
+        install = { colorscheme = { "tokyonight", "habamax" } },
     })
 end
-vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-    spec = {
-        { import = "plugins" },
-    },
-    install = { colorscheme = { "tokyonight", "habamax" } },
-})
+return M
