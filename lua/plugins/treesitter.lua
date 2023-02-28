ec3dd5f4ee801584c6e2b8c0ad68fe63ba28b277
xuchengpeng <330476629@qq.com>
2023-02-28 08:26:08 +0800

    treesitter: ensure installed regex

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -14,6 +14,7 @@ return {
                     "markdown",
                     "markdown_inline",
                     "python",
+                    "regex",
                     "vim",
                 },
                 sync_install = false,
