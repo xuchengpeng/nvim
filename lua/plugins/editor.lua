f70e3487dd2e58d5508e30c78209b2a3195b2bff
xuchengpeng <330476629@qq.com>
2023-03-04 15:17:22 +0800

    feat(editor): better splitting/joining blocks

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,30 +1,42 @@
 return {
-    "RRethy/vim-illuminate",
-    lazy = true,
-    event = "BufReadPost",
-    config = function()
-        require("illuminate").configure({
-            providers = {
-                "lsp",
-                "treesitter",
-                "regex",
-            },
-            delay = 200,
-            filetypes_denylist = {
-                "dirvish",
-                "fugitive",
-                "alpha",
-                "NvimTree",
-                "lazy",
-                "neogitstatus",
-                "Trouble",
-                "lir",
-                "Outline",
-                "spectre_panel",
-                "toggleterm",
-                "DressingSelect",
-                "TelescopePrompt",
-            },
-        })
-    end
+    {
+        "RRethy/vim-illuminate",
+        lazy = true,
+        event = "BufReadPost",
+        config = function()
+            require("illuminate").configure({
+                providers = {
+                    "lsp",
+                    "treesitter",
+                    "regex",
+                },
+                delay = 200,
+                filetypes_denylist = {
+                    "dirvish",
+                    "fugitive",
+                    "alpha",
+                    "NvimTree",
+                    "lazy",
+                    "neogitstatus",
+                    "Trouble",
+                    "lir",
+                    "Outline",
+                    "spectre_panel",
+                    "toggleterm",
+                    "DressingSelect",
+                    "TelescopePrompt",
+                },
+            })
+        end
+    },
+    {
+        "Wansmer/treesj",
+        lazy = true,
+        cmd = { "TSJToggle", "TSJSplit", "TSJJoin" },
+        opts = { use_default_keymaps = false },
+        keys = {
+            { "<leader>cj", "<cmd>TSJJoin<cr>",  desc = "Join" },
+            { "<leader>cs", "<cmd>TSJSplit<cr>", desc = "Split" },
+        },
+    },
 }
