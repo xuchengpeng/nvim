4eaa1acc5ebd05ffd2dd3ebcbecb8e22de2c07e8
xuchengpeng <330476629@qq.com>
2023-03-06 13:16:46 +0800

    fix(alpha): improve menu

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -20,12 +20,12 @@ return {
             }
 
             dashboard.section.buttons.val = {
-                dashboard.button("e", "  > New file", "<cmd>ene <BAR> startinsert <CR>"),
-                dashboard.button("f", "  > Find files", "<cmd>Telescope find_files<CR>"),
-                dashboard.button("g", "  > Grep word", "<cmd>Telescope live_grep<CR>"),
-                dashboard.button("r", "  > Recent files", "<cmd>Telescope oldfiles<CR>"),
+                dashboard.button("e", "  > New File", "<cmd>ene <BAR> startinsert <CR>"),
+                dashboard.button("f", "  > Find File", "<cmd>Telescope find_files<CR>"),
+                dashboard.button("g", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
+                dashboard.button("r", "  > Recents", "<cmd>Telescope oldfiles<CR>"),
                 dashboard.button("s", "  > Settings", "<cmd>e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-                dashboard.button("u", "  > Update plugins", "<cmd>Lazy update<CR>"),
+                dashboard.button("u", "  > Update Plugins", "<cmd>Lazy update<CR>"),
                 dashboard.button("q", "  > Quit NVIM", "<cmd>quit<CR>"),
             }
 
