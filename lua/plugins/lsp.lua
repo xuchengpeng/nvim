1a181135cda481e257bf320af8af61a4693648b8
xuchengpeng <330476629@qq.com>
2023-03-07 13:41:31 +0800

    fix(lsp): remove Lspsaga

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -25,6 +25,43 @@ return {
       })
       require("lspconfig").clangd.setup({})
     end,
+    keys = {
+      { "K", vim.lsp.buf.hover, desc = "Hover doc" },
+      { "<leader>l", desc = "+Lsp" },
+      { "<leader>la", vim.lsp.buf.code_action, desc = "Code Action" },
+      { "<leader>ld", "<cmd>Telescope lsp_definitions<cr>", desc = "Goto Definition" },
+      {
+        "<leader>lf",
+        function()
+          vim.lsp.buf.format({
+            filter = function(client)
+              local filetype = vim.bo.filetype
+              local n = require("null-ls")
+              local s = require("null-ls.sources")
+              local method = n.methods.FORMATTING
+              local available_formatters = s.get_available(filetype, method)
+
+              if #available_formatters > 0 then
+                return client.name == "null-ls"
+              elseif client.supports_method("textDocument/formatting") then
+                return true
+              else
+                return false
+              end
+            end,
+          })
+        end,
+        desc = "Format",
+      },
+      { "<leader>li", "<cmd>Telescope lsp_implementations<cr>", desc = "Goto Implementation" },
+      { "<leader>lr", "<cmd>Telescope lsp_references<cr>", desc = "References" },
+      { "<leader>lR", vim.lsp.buf.rename, desc = "Rename" },
+      { "<leader>lt", "<cmd>Telescope lsp_type_definitions<cr>", desc = "Goto Type Definition" },
+      { "<leader>ls", "<cmd>Telescope lsp_document_symbols<cr>", desc = "Document Symbols" },
+      { "<leader>lS", "<cmd>Telescope lsp_workspace_symbols<cr>", desc = "Workspace Symbols" },
+      { "<leader>ln", vim.diagnostic.goto_next, desc = "Diagnostic next" },
+      { "<leader>lp", vim.diagnostic.goto_prev, desc = "Diagnostic prev" },
+    },
   },
   {
     "jose-elias-alvarez/null-ls.nvim",
@@ -39,27 +76,5 @@ return {
         },
       })
     end,
-  },
-  {
-    "glepnir/lspsaga.nvim",
-    lazy = true,
-    event = { "BufRead", "BufNewFile" },
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("lspsaga").setup()
-    end,
-    keys = {
-      { "K", "<cmd>Lspsaga hover_doc<cr>", desc = "Hover doc" },
-      { "<leader>l", desc = "+Lsp" },
-      { "<leader>la", "<cmd>Lspsaga code_action<cr>", desc = "Code action" },
-      { "<leader>ld", "<cmd>Lspsaga goto_definition<cr>", desc = "Goto definition" },
-      { "<leader>lf", "<cmd>lua vim.lsp.buf.format()<cr>", desc = "Format" },
-      { "<leader>lh", "<cmd>Lspsaga lsp_finder<cr>", desc = "LSP finder" },
-      { "<leader>lo", "<cmd>Lspsaga outline<cr>", desc = "Outline" },
-      { "<leader>lr", "<cmd>Lspsaga rename<cr>", desc = "Rename" },
-      { "<leader>lt", "<cmd>Lspsaga goto_type_definition<cr>", desc = "Goto type definition" },
-      { "<leader>ln", "<cmd>Lspsaga diagnostic_jump_next<cr>", desc = "Diagnostic next" },
-      { "<leader>lp", "<cmd>Lspsaga diagnostic_jump_prev<cr>", desc = "Diagnostic prev" },
-    },
   },
 }
