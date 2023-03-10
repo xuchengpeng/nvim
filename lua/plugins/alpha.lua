eed1575701216034a458b7573203894cef7839b2
xuchengpeng <330476629@qq.com>
2023-03-10 16:12:23 +0800

    fix(alpha): customize menu icons

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 M.setup = function()
+  local ui = require("utils.icons").ui
   local alpha = require("alpha")
   local dashboard = require("alpha.themes.dashboard")
 
@@ -16,13 +17,13 @@ M.setup = function()
   }
 
   dashboard.section.buttons.val = {
-    dashboard.button("e", "  > New File", "<cmd>ene <BAR> startinsert <CR>"),
-    dashboard.button("f", "  > Find File", "<cmd>Telescope find_files<CR>"),
-    dashboard.button("g", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-    dashboard.button("r", "  > Recents", "<cmd>Telescope oldfiles<CR>"),
-    dashboard.button("s", "  > Settings", "<cmd>e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-    dashboard.button("u", "  > Update Plugins", "<cmd>Lazy update<CR>"),
-    dashboard.button("q", "  > Quit NVIM", "<cmd>quit<CR>"),
+    dashboard.button("e", ui.NewFile .. "  > New File", "<cmd>ene <BAR> startinsert <CR>"),
+    dashboard.button("f", ui.FindFile .. "  > Find File", "<cmd>Telescope find_files<CR>"),
+    dashboard.button("g", ui.FindText .. "  > Find Word", "<cmd>Telescope live_grep<CR>"),
+    dashboard.button("r", ui.History .. "  > Recents", "<cmd>Telescope oldfiles<CR>"),
+    dashboard.button("s", ui.Gear .. "  > Settings", "<cmd>e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+    dashboard.button("u", ui.Update .. "  > Update Plugins", "<cmd>Lazy update<CR>"),
+    dashboard.button("q", ui.Close .. "  > Quit NVIM", "<cmd>quit<CR>"),
   }
 
   alpha.setup(dashboard.opts)
--- lua/utils/icons.lua
+++ lua/utils/icons.lua
@@ -127,6 +127,7 @@ return {
     TriangleShortArrowLeft = "",
     TriangleShortArrowRight = "",
     TriangleShortArrowUp = "",
+    Update = "",
   },
   diagnostics = {
     BoldError = "",
