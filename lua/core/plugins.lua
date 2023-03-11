6326464db8c9b89361b86d3d0e8ee72f0fe7f297
xuchengpeng <330476629@qq.com>
2023-03-11 16:38:44 +0800

    fix(cmp): add friendly-snippets

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -83,12 +83,20 @@ return {
     lazy = true,
     event = { "InsertEnter" },
     dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "hrsh7th/cmp-buffer" },
+      { "hrsh7th/cmp-path" },
+      { "hrsh7th/cmp-cmdline" },
+      {
+        "L3MON4D3/LuaSnip",
+        dependencies = {
+          "rafamadriz/friendly-snippets",
+          config = function()
+            require("luasnip.loaders.from_vscode").lazy_load()
+          end,
+        },
+      },
+      { "saadparwaiz1/cmp_luasnip" },
     },
     config = function()
       require("plugins.cmp").setup()
