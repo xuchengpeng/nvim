e83b660590fd845d1e4371c664cbfcd6ef59b775
xuchengpeng <330476629@qq.com>
2023-03-14 17:37:38 +0800

    fix: add function descriptions

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -1,19 +1,28 @@
 local M = {}
 
+---Get user configuration directory
 M.get_config_dir = function()
   return vim.fn.stdpath("config")
 end
 
+---Get user data directory
 M.get_data_dir = function()
   return vim.fn.stdpath("data")
 end
 
+---Get runtime directory: temporary, local storage for sockets, named pipes, etc.
 M.get_runtime_dir = function()
   return vim.fn.stdpath("run")
 end
 
+---Get session state directory: storage for file drafts, swap, undo, shada
 M.get_state_dir = function()
   return vim.fn.stdpath("state")
+end
+
+---Get cache directory: arbitrary temporary storage for plugins, etc
+M.get_cache_dir = function()
+  return vim.fn.stdpath("cache")
 end
 
 local path_sep = vim.loop.os_uname().version:match("Windows") and "\\" or "/"
@@ -44,7 +53,7 @@ function M.conditional_func(func, condition, ...)
   end
 end
 
---- Serve a notification with a title of AstroNvim
+--- Serve a notification with a title of Nvim
 -- @param msg the notification body
 -- @param type the type of the notification (:help vim.log.levels)
 -- @param opts table of nvim-notify options to use (:help notify-options)
