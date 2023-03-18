37faee76ff87812f79900d1a7b29826f6762a360
xuchengpeng <330476629@qq.com>
2023-03-18 13:32:54 +0800

    fix(null-ls): run eslint.cmd on Windows

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -58,16 +58,18 @@ M.setup = function()
 
   require("core.keymaps").set_keymaps("n", "K", { vim.lsp.buf.hover, { desc = "Hover" } })
 
+  local on_windows = vim.loop.os_uname().version:match("Windows")
+  local eslint_opts = { command = on_windows and "eslint.cmd" or "eslint" }
   local null_ls = require("null-ls")
   null_ls.setup({
     sources = {
-      null_ls.builtins.code_actions.eslint,
+      null_ls.builtins.code_actions.eslint.with(eslint_opts),
       null_ls.builtins.code_actions.gitsigns,
-      null_ls.builtins.diagnostics.eslint,
+      null_ls.builtins.diagnostics.eslint.with(eslint_opts),
       null_ls.builtins.formatting.black,
-      null_ls.builtins.formatting.eslint,
+      null_ls.builtins.formatting.eslint.with(eslint_opts),
       null_ls.builtins.formatting.prettier.with({
-        command = vim.loop.os_uname().version:match("Windows") and "prettier.cmd" or "prettier",
+        command = on_windows and "prettier.cmd" or "prettier",
         filetypes = { "html", "json", "jsonc", "yaml", "markdown" },
       }),
       null_ls.builtins.formatting.shfmt,
