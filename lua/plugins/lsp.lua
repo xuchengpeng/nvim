dcf5c1455a3351730bd1bfdc4d23d08472a19436
xuchengpeng <330476629@qq.com>
2023-03-18 11:34:22 +0800

    fix(null-ls): add eslint support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -61,10 +61,13 @@ M.setup = function()
   local null_ls = require("null-ls")
   null_ls.setup({
     sources = {
+      null_ls.builtins.code_actions.eslint,
       null_ls.builtins.code_actions.gitsigns,
-      null_ls.builtins.formatting.stylua,
-      null_ls.builtins.formatting.shfmt,
+      null_ls.builtins.diagnostics.eslint,
       null_ls.builtins.formatting.black,
+      null_ls.builtins.formatting.eslint,
+      null_ls.builtins.formatting.shfmt,
+      null_ls.builtins.formatting.stylua,
     },
   })
 end
