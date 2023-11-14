d2174312dc7ebfc1e3c6fa5f53fba76d085ec7e0
xuchengpeng <330476629@qq.com>
2023-11-14 18:55:14 +0800

    fix(indent-blankline): do not show underline of the scope

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -2,6 +2,7 @@ local M = {}
 
 function M.setup()
   require("ibl").setup({
+    scope = { show_start = false, show_end = false },
     exclude = {
       buftypes = { "terminal", "nofile", "quickfix", "prompt" },
       filetypes = { "checkhealth", "help", "lazy", "lspinfo", "man", "mason", "neo-tree", "Trouble", "text" },
