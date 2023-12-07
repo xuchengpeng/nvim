689d808b2ac2cb907951e5cd2bc2162e2fe4f892
xuchengpeng <330476629@qq.com>
2023-12-07 11:22:50 +0800

    fix(notify): add timeout,max_width options

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -3,6 +3,8 @@ local M = {}
 function M.setup()
   local notify = require("notify")
   notify.setup({
+    timeout = 3000,
+    max_width = 80,
     on_open = function(win)
       vim.api.nvim_win_set_config(win, { border = "single" })
     end,
