f0b5b2a28d57500c6ec649bc2fc25205ba94b345
xuchengpeng <330476629@qq.com>
2024-08-09 13:19:47 +0800

    fix(editor): remove colorizer

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -24,21 +24,6 @@ return {
     config = true,
   },
   {
-    "NvChad/nvim-colorizer.lua",
-    lazy = true,
-    event = { "User FileOpened" },
-    config = function()
-      require("colorizer").setup({
-        user_default_options = {
-          names = false,
-        },
-      })
-      vim.defer_fn(function()
-        require("colorizer").attach_to_buffer(0)
-      end, 0)
-    end,
-  },
-  {
     "RRethy/vim-illuminate",
     lazy = true,
     event = { "User FileOpened" },
